a3f6ee99d63bf105373e759637c2248701bd73af
Rocklin K S <rocklin2603@gmail.com>
2025-03-24 21:21:03 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -33,38 +33,30 @@ def percentage(a,b):
     
 total = None
 
-while True:
-    if total is None:
-        a=int(input("Enter the first number: "))
-    else:
-        a=total
-
-    b=int(input("Enter the second number: "))
-    choice = int(input("Enter your choice either 1 or 2 or 3 or 4 or 5 or 6: "))
-    
     if choice == 1:
         total=add(a,b)
         print(f"The addition of {a} and {b} is {total}")
     elif choice == 2:
         total=sub(a,b)
-        print(total)
+        print(f"The subtraction of {a} from {b} is {total}")
     elif choice == 3:
         total=mul(a,b)
-        print(total)
+        print(f"The multiplication of {a} and {b} is {total}")
     elif choice == 4:
         totalt=div(a,b)
-        print(total)
+        print(f"The division of {a} by {b} is {total}")
     elif choice == 5:
         totalt=percentage(a,b)
-        print(total)
+        print(f"The {a} percentage of {b} is {total}")
     elif choice == 6:
-        break
+
+        exit()
     else:
         print("Invalid Choice")
     next=input("Do you want to use operation Yy/Nn ")
     if next=='y' or next=='Y':
         continue
     elif next=='n' or next=="N":
-        exit()
+        break
     else:
         print("Enter a valid input")
